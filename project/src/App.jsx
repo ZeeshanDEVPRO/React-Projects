bb61ee3bbbde08ac8b1671b91ed92e9e6d104d7c
Zeeshan <professionalid2003@gmail.com>
2023-10-19 11:44:02 +0530

    files added

---
+++ project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import StartGame from "./components/StartGame";
+import Gameplay from "./components/Gameplay";
+
+function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
+  return (
+    <>{isGameStarted ? <Gameplay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
+}
+
+export default App;
